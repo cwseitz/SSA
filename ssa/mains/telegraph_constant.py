43cc3c64002cc0153885f18dcbc9aed7a29548a8
cwseitz <cwseitz2@gmail.com>
2023-02-05 02:16:16 -0500

    update

--- ssa/mains/telegraph_constant.py
+++ ssa/mains/telegraph_constant.py
@@ -19,7 +19,6 @@ def get_discrete_values(times, x1, x2, x3, Nt, T=5):
         for i in range(1, Nt):
             dt = discrete_times[i] - discrete_times[i-1]
             time_idx = np.argwhere((times >= discrete_times[i-1]) & (times < discrete_times[i]))
-            #print(times[time_idx])
             if np.any(time_idx):
                 time_idx = np.squeeze(time_idx)
                 discrete_values[0, i] = x1[time_idx]
@@ -28,17 +27,21 @@ def get_discrete_values(times, x1, x2, x3, Nt, T=5):
             else:
                 discrete_values[0, i] = discrete_values[0, i-1]
                 discrete_values[1, i] = discrete_values[1, i-1] 
-                discrete_values[2, i] = discrete_values[2, i-1]             
+                discrete_values[2, i] = discrete_values[2, i-1]
+    else:
+        discrete_values[0,:] = 1
+        discrete_values[1,:] = 0
+        discrete_values[2,:] = 0                     
     return discrete_times, discrete_values
 
 
 end_time = 5.0
 k_on = 1.0
 k_off = 1.0
-ksyn = 1.0
+ksyn = 3.0
 kdeg = 1.0
 time_step = 0.005
-Nreps = 100
+Nreps = 200
 dt = 0.1
 Nt = 500
 Nsamp = 20000
@@ -52,10 +55,10 @@ X = np.zeros((Nreps,3,Nsamp))
 for n in range(Nreps):
     print(f'Simulating Rep {n}')
     x1, x2, x3, times = telegraph_constant([end_time,k_on,k_off,ksyn,kdeg,Nt])
-    #print(times)
     values = [x1, x2, x3]
     discrete_times, discrete_values = get_discrete_values(times, x1, x2, x3, Nsamp)
     X[n,:,:] = discrete_values
+
 
 pct_on = np.mean(X[:,1,:],axis=0)
 pct_off = np.mean(X[:,0,:],axis=0)
